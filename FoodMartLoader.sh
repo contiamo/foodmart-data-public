0758da6fcc8d6d689bb521c465258512ff893d10
Bram Geron <bram@contiamo.com>
2019-09-12 15:39:23 +0200

    sqlserver: Don't override credentials given on command line; give creds explicitly. (#6)

--- FoodMartLoader.sh
+++ FoodMartLoader.sh
@@ -65,8 +65,6 @@ configure_db()	{
 			db_driver="net.sourceforge.jtds.jdbc.Driver"
 			db_url="jdbc:jtds:sqlserver://${db_host}/foodmart"
 			# sqlserver has strict password requirements
-			db_user=foodmart
-			db_pass=F00dmartpass
 			;;
 
 		(sybase)
--- docker/sqlserver/gen-docker.sh
+++ docker/sqlserver/gen-docker.sh
@@ -4,12 +4,17 @@
 #
 #     bash gen-docker.sh || (docker stop mssql-temp; docker rm mssql-temp)
 #
-# If successful, this will upload a fresh sqlserver+foodmart image to the container registry.
+# If successful, this will save a fresh sqlserver+foodmart image to the local container storage.
+# Upload with `docker push`.
 
 DOCKER_SRC_IMG=mcr.microsoft.com/mssql/server:2017-latest
-DOCKER_TARGET_IMG=eu.gcr.io/dev-and-test-env/foodmart-mssql:v0.0.1
+DOCKER_TARGET_IMG=eu.gcr.io/dev-and-test-env/foodmart-mssql:v0.0.3
 SA_PASSWORD=my_SA_pwd
 TEMP_CONTAINER=mssql-temp
+
+# must match init.sql
+USER_USER=foodmart
+USER_PASSWORD=F00dmartpass
 
 docker version > /dev/null || exit 1
 
@@ -27,7 +32,7 @@ docker exec -it $TEMP_CONTAINER /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -
 docker exec -it $TEMP_CONTAINER /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -Q "$(grep -v ^\\s*-- init.sql )"
 
 # Load data
-../../FoodMartLoader.sh --db sqlserver --db-host localhost
+../../FoodMartLoader.sh --db sqlserver --db-host localhost --db-user $USER_USER --db-pass $USER_PASSWORD
 
 docker stop $TEMP_CONTAINER
 
